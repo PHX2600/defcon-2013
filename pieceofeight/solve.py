a931e638c8878a399a946989262d1425aac1d209
AltF4 <altf4@phx2600.org>
2013-06-14 21:02:55 -0700

    + ‘pieceofeight/solve.py’

---
+++ pieceofeight/solve.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+import socket
+
+def parsePuzzle(s):
+    puzList = []
+    num = 0
+    lines = s.recv(1024).split('\n')
+    i = 0
+    for line in lines:
+        if( (i == 3) or (i == 8) or (i == 13) ):
+            if(line[3] == ' '):
+                num = 0
+            else:
+                num = int(line[3])
+            print line
+
+            puzList.append(num)
+
+            if(line[9] == ' '):
+                num = 0
+            else:
+                num = int(line[9])
+
+            puzList.append(num)
+
+            if(line[15] == ' '):
+                num = 0
+            else:
+                num = int(line[15])
+
+            puzList.append(num)
+
+        i = i + 1
+    return puzList
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("pieceofeight2.shallweplayaga.me", 8273))
+puzList = parsePuzzle(s)
+print puzList
+
