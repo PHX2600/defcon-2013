c21e432dfb2c4e8978d17333e93645047e9813af
altf4 <altf4@phx2600.org>
2013-06-15 12:42:03 -0700

    + ‘policebox/Dump’ + ‘policebox/core’ + ‘policebox/dump’ + ‘policebox/mod’ + ‘policebox/policebox’ + ‘policebox/source.c’ + ‘policebox/str-core’ + ‘policebox/str-police’ + ‘thyself/Dump’ + ‘thyself/client’ + ‘thyself/dump’ + ‘thyself/str’

---
+++ policebox/source.c
@@ -0,0 +1,39 @@
+#include "stdio.h"
+
+void tty_exit()
+{
+    tty_reset();
+}
+
+void tty_reset()
+{
+    tcsetattr(0, )
+
+}
+
+void tty_raw()
+{
+    
+}
+
+
+int main()
+{
+
+    atexit(tty_exit);
+
+    tty_raw();
+
+    printf("The key is: ");
+    
+    for(int i = 0; i <= 63; i++)
+    {
+        char in = getchar();
+        if(in != "\r")
+            continue;
+        puts("\r");
+        break;
+    }
+
+
+}
