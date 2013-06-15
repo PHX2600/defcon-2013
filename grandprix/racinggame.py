1ecc16b589b49030a9384ef47a41e909aa27eaba
Uberschnitzel <uberschnitzel@gmail.com>
2013-06-15 16:40:01 -0700

    + ‘grandprix/racinggame.py’

---
+++ grandprix/racinggame.py
@@ -0,0 +1,61 @@
+'''
+racinggame.py
+
+u - player
+~ - snake/crash
+T - tree/crash
+P - person/crash
+Z - zebra/crash
+r - rock/crash
+c - car/crash
+X - road block/ crash
+| - wall/crash - restrict movement to the 5 positions between the walls
+
+At any give time it needs to be able to predict 5 steps ahead.
+Needs to be moving forward at every step.
+Avoid all objects in front of you between the walls.
+
+Starting screen:
+
+|-----|
+|     |
+|     |
+|     |
+|     |
+|     |
+|     |
+|     |
+|     |
+|  u  |
+|-----|
+
+'''
+
+import random
+import math
+import socket
+
+def moveForward(s):
+    s.send("\n")
+
+def moveLeft(s):
+    s.send("l\n")
+
+def moveRight(s):
+    s.send("r\n")
+
+def parseTrack(s):
+	trackList = []
+	num = 0
+	lines = s.recv(1024).split('\n')
+	print lines
+	i = 0
+
+def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("grandprix.shallweplayaga.me", 2038))
+    moveForward(s)
+    parseTrack(s)
+
+if __name__ == "__main__":
+    main()
