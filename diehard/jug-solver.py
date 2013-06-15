6140effd7549feafdb6f7a19661fc72bfc99c3ec
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-06-15 00:11:10 -0700

    / ‘diehard/jug-solver.py’

--- diehard/jug-solver.py
+++ diehard/jug-solver.py
@@ -2,7 +2,9 @@
 
 import socket
 
-// Initilize socket connection
+# Initilize socket connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('diehard.shallweplayaga.me', 4001))
 
+# Go to jug problem room
+print s.recv()
