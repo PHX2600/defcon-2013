62bcd493c2fda1cce1ff3ef529fc089ea9d49c53
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-06-15 00:58:10 -0700

    / ‘diehard/jug-solver.py’

--- diehard/jug-solver.py
+++ diehard/jug-solver.py
@@ -30,16 +30,6 @@ time.sleep(.2)
 
 print s.recv(4096)
 
-# Get red jug value
-s.send('look red jug\n')
-time.sleep(.2)
-str = s.recv(1024)
-red = [int(i) for i in str.split() if i.isdigit()]
-
-redCurrent = red[0]
-redMAx     = red[1]
-
-print red
 
 # Get blue jug value
 s.send('look blue jug\n')
@@ -52,6 +42,19 @@ blueMax     = blue[1]
 
 print blue
 
+
+# Get red jug value
+s.send('look red jug\n')
+time.sleep(.2)
+str = s.recv(1024)
+red = [int(i) for i in str.split() if i.isdigit()]
+
+redCurrent = red[0]
+redMAx     = red[1]
+
+print red
+
+
 # Get target value
 s.send('look inscription\n')
 time.sleep(.2)
@@ -62,8 +65,16 @@ target = targetArray[0]
 
 print target
 
-
-blueCurrent = 0
+# Pick up the damned jugs
+s.send('get blue jug')
+s.send('get red jug')
 
 while (redCurrent != target && blueCurrent != target):
-    s.send('')
+
+    if (blueMax < redMax):
+
+        s.send('')
+
+
+    if (redMax < blueMax):
+
