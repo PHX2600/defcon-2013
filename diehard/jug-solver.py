cfa02af1462ea3ac8d5559983c0b883f0421232f
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-06-15 00:46:54 -0700

    / ‘diehard/jug-solver.py’

--- diehard/jug-solver.py
+++ diehard/jug-solver.py
@@ -38,7 +38,7 @@ redArray = [int(i) for i in str.split() if i.isdigit()]
 
 red = redArray[1]
 
-print 'red = ' + red
+print 'red = ' + str(red)
 
 # Get blue jug value
 s.send('look blue jug\n')
@@ -48,7 +48,7 @@ blueArray = [int(i) for i in str.split() if i.isdigit()]
 
 blue = blueArray[1]
 
-print 'blue = ' + blue
+print 'blue = ' + str(blue)
 
 # Get target value
 s.send('look inscription\n')
