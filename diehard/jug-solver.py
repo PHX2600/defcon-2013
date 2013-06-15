3bfc0d8c5316a489e8e6d88ededdb6c5b273efee
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-06-15 00:18:02 -0700

    / ‘diehard/jug-solver.py’

--- diehard/jug-solver.py
+++ diehard/jug-solver.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import socket
+import socket, time
 
 # Initilize socket connection
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -8,15 +8,26 @@ s.connect(('diehard.shallweplayaga.me', 4001))
 
 # Go to jug problem room
 s.send('n\n')
+time.sleep(1)
 s.send('n\n')
+time.sleep(1)
 s.send('n\n')
+time.sleep(1)
 s.send('n\n')
+time.sleep(1)
 s.send('n\n')
+time.sleep(1)
 s.send('n\n')
+time.sleep(1)
 s.send('e\n')
+time.sleep(1)
 s.send('n\n')
+time.sleep(1)
 s.send('w\n')
+time.sleep(1)
 s.send('n\n')
+time.sleep(1)
 s.send('n\n')
+time.sleep(1)
 
 print s.recv(4096)
