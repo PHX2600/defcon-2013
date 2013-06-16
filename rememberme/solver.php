e5543dd4a560177cd571353a426bc1685bede6d2
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-06-15 17:35:39 -0700

    - ‘rememberme/solver.php’

--- rememberme/solver.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $time = strtotime('Sun, 16 Jun 2013 00:24:09 GMT');
-
-    $base64_cipher = "737NPHkg8wQiBvnVBIuzN/CewtKu/DHlHY/coShCEHmBzl7jP2nOFvZk+K9kJ35FJDDSnxZuMfbhO4WbsNO7iQ==";
-    $cipher = base64_decode($base64_cipher);
-
-
-
