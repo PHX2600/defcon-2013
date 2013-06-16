5d234da81ac7c1d103fc97f9be07fbcb4109f4b4
chunderstruck <dchun529@gmail.com>
2013-06-15 19:09:24 -0700

    / ‘grandprix/racinggame.py’

--- grandprix/racinggame.py
+++ grandprix/racinggame.py
@@ -58,8 +58,9 @@ def moveRight(s):
 
 def parseTrack(s):
     trackList = []
-    lines = s.recv(1024)
+    data = s.recv(1024)
     if 'Press return to start' in line:
+      
     num = 0
     lines = s.recv(1024).split('\n')
     print lines
