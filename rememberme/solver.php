47baa2eaf68065be1e5a81c3d0d5b4ffceed4ed4
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2013-06-15 17:26:30 -0700

    / ‘rememberme/solver.php’

--- rememberme/solver.php
+++ rememberme/solver.php
@@ -1,5 +1,9 @@
 <?php
 
-    $base64_cipher = "737NPHkg8wQiBvnVBIuzN/CewtKu/DHlHY/coShCEHmBzl7jP2nOFvZk+K9kJ35FJDDSnxZuMfbhO4WbsNO7iQ==";
-
     $time = strtotime('Sun, 16 Jun 2013 00:24:09 GMT');
+
+    $base64_cipher = "737NPHkg8wQiBvnVBIuzN/CewtKu/DHlHY/coShCEHmBzl7jP2nOFvZk+K9kJ35FJDDSnxZuMfbhO4WbsNO7iQ==";
+    $cipher = base64_decode($base64_cipher);
+
+
+
